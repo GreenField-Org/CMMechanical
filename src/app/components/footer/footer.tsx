3493b13ef86cc6e8b4028b629e3ebec56cdcb396
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-17 23:41:29 -0500

    adding mobile styling on footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -5,11 +5,11 @@ import { Suspense } from "react";
 export default function Footer() {
     return (
         <footer className="text-white p-10">
-            <div className="flex justify-evenly pb-4">
+            <div className="grid text-center lg:w-full lg:mb-5 lg:grid-cols-3 lg:text-left mb-6">
                 <Suspense>
-                    <Image src="/CMMechanical_logo.png" width={500} height={500} alt="C&M Mechanical Logo"/>
+                    <Image src="/CMMechanical_logo.png" width={300} height={300} alt="C&M Mechanical Logo" className="my-0 mx-auto mb-6"/>
                 </Suspense>
-                <div>
+                <div className="group grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mb-6">
                     <Link href="/about">About Us</Link>
                     <Link href="/services">Services</Link>
                     <Link href="/contact">Contact</Link>
@@ -21,7 +21,7 @@ export default function Footer() {
             </div>
             <hr className="bg-white"/>
             <p className="flex justify-center p-5">
-                &copy; {new Date().getFullYear()} GreenField Dev.
+                &copy; {new Date().getFullYear()} GreenField Dev
             </p>
         </footer>
     )
