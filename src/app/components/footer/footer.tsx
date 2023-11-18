9d27fe2a20b733940c0f91c678e62a801c32a4fa
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-18 16:20:45 -0500

    fixing hydration error on the footer and adding transition colors for light and dark mode

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,11 +4,9 @@ import { Suspense } from "react";
 
 export default function Footer() {
     return (
-        <footer className="text-white p-10">
+        <footer className="reansition-colos p-10">
             <div className="grid text-center lg:w-full lg:mb-5 lg:grid-cols-3 lg:text-left mb-6">
-                <Suspense>
                     <Image src="/CMMechanical_logo.png" width={300} height={300} alt="C&M Mechanical Logo" className="my-0 mx-auto mb-6"/>
-                </Suspense>
                 <div className="group grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mb-6">
                     <Link href="/about">About Us</Link>
                     <Link href="/services">Services</Link>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Footer />
+      <body className={inter.className}>
+        {children}
+        <Footer />  
+      </body>
     </html>
   )
 }
