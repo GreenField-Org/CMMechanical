991dce4e3a535ab17967b323dc76beba888fa45d
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-11-22 22:38:00 -0500

    corrections to footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 export default function Footer() {
     return (
-        <footer className="reansition-colos p-10">
+        <footer className="transition-colors p-10">
             <div className="grid text-center lg:w-full lg:mb-5 lg:grid-cols-3 lg:text-left mb-6">
                     <Image src="/CMMechanical_logo.png" width={300} height={300} alt="C&M Mechanical Logo" className="my-0 mx-auto mb-6"/>
                 <div className="group grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mb-6">
