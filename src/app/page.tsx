2b2bc65323152d3e0def2d847dcfc9b5b726a51b
Daniel Vigil <daniel.vigil@fusionmedstaff.com>
2023-12-11 22:21:43 -0500

    closes #12 carousel and testimonial components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Cta from "./components/cta/cta";
 import Faq from "./components/faq/faq";
+import Carousel from "./components/carousel/carousel";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Carousel />
       <Faq description={"this is a test"} href="/about" />
       <Cta />
     </main>
