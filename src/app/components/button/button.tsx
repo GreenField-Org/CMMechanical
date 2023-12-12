b1a36b2dfe02602326ce4356582388732b9ff436
Daniel Vigil <daniel.vigil@fusionmedstaff.com>
2023-12-11 21:35:30 -0500

    closes #4 navbar component

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -2,14 +2,20 @@ interface ButtonProps {
   buttonText: string;
   onClick?: () => void;
   variant?: "primary" | "secondary";
+  fullwidth?: boolean;
 }
 
-export default function Button({ buttonText, variant, onClick }: ButtonProps) {
+export default function Button({
+  buttonText,
+  variant,
+  fullwidth,
+  onClick,
+}: ButtonProps) {
   return (
     <button
       className={`flex justify-center items-center gap-2 border px-6 py-3 border-solid border-primary ${
         variant === "primary" ? "bg-primary" : "bg-none"
-      } rounded-2xl hover:bg-secondary`}
+      } rounded-2xl hover:bg-secondary ${fullwidth ? "w-full" : ""}`}
       onClick={onClick}
     >
       {buttonText}
