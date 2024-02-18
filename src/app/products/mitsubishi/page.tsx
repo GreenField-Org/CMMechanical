9a340c7b4deee1acc29109496bbe67efd309f70b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-02-17 20:30:17 -0500

    updating border of iframe

--- src/app/products/mitsubishi/page.tsx
+++ src/app/products/mitsubishi/page.tsx
@@ -5,5 +5,5 @@ export const metadata: Metadata = {
 };
 
 export default function MitsubishiPage() {
-  return <iframe src="https://ebandlmarketing.com/prodconnect-test/mitsubishi/" rel="nofollow" width="100%" height="850px"></iframe>;
+  return <iframe src="https://ebandlmarketing.com/prodconnect-test/mitsubishi/" rel="nofollow" width="100%" height="850px" className="rounded-lg shadow-lg"></iframe>;
 }
