b4aa3a7f7ec99524a9cd1f2686420aa40bfecb1b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-02-20 23:56:12 -0500

    updating the footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,8 +4,9 @@ import Socials from "../socials/socials";
 
 export default function Footer() {
   return (
-    <footer className="transition-colors p-10">
+    <footer className="transition-colors py-10 px-5">
       <div className="grid text-center lg:w-full lg:mb-5 lg:grid-cols-3 lg:text-left mb-6">
+        <div className="flex flex-col justify-center items-center gap-2 mb-6">
         <Image
           src="/CMMechanical_logo.png"
           width={300}
@@ -13,20 +14,23 @@ export default function Footer() {
           alt="C&M Mechanical Logo"
           className="my-0 mx-auto mb-6"
         />
+        <h4>Phone: <a href="tel:518-768-8800">518-768-8800</a></h4>
+        <h4>Email: <a href="mailto:office@cmmechanicalhvac.com">office@cmmechanicalhvac.com</a></h4>
+        </div>
         <div className="group grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mb-6">
-          <Link className="hover:text-tertiary" href="/about">
+          <Link className="hover:text-tertiary mb-1" href="/about">
             About Us
           </Link>
-          <Link className="hover:text-tertiary" href="/services">
+          <Link className="hover:text-tertiary mb-1" href="/services">
             Services
           </Link>
-          <Link className="hover:text-tertiary" href="/financing">
+          <Link className="hover:text-tertiary mb-1" href="/financing">
             Financing
           </Link>
-          <Link className="hover:text-tertiary" href="/contact">
+          <Link className="hover:text-tertiary mb-1" href="/contact">
             Contact
           </Link>
-          <Link className="hover:text-tertiary" href="/faq">
+          <Link className="hover:text-tertiary mb-1" href="/faq">
             FAQ
           </Link>
         </div>
