01b712481cf62702a1e3b680a1c733d8908172a0
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-03-02 17:15:31 -0500

    updating the footer component

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -7,15 +7,22 @@ export default function Footer() {
     <footer className="transition-colors py-10 px-5">
       <div className="grid text-center lg:w-full lg:mb-5 lg:grid-cols-3 lg:text-left mb-6">
         <div className="flex flex-col justify-center items-center gap-2 mb-6">
-        <Image
-          src="/CMMechanical_logo.png"
-          width={300}
-          height={300}
-          alt="C&M Mechanical Logo"
-          className="my-0 mx-auto mb-6"
-        />
-        <h4>Phone: <a href="tel:518-768-8800">518-768-8800</a></h4>
-        <h4>Email: <a href="mailto:office@cmmechanicalhvac.com">office@cmmechanicalhvac.com</a></h4>
+          <Image
+            src="/CMMechanical_logo.png"
+            width={300}
+            height={300}
+            alt="C&M Mechanical Logo"
+            className="my-0 mx-auto mb-6"
+          />
+          <h4>
+            Phone: <a href="tel:518-768-8800">518-768-8800</a>
+          </h4>
+          <h4>
+            Email:{" "}
+            <a href="mailto:office@cmmechanicalhvac.com">
+              office@cmmechanicalhvac.com
+            </a>
+          </h4>
         </div>
         <div className="group grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mb-6">
           <Link className="hover:text-tertiary mb-1" href="/about">
@@ -38,7 +45,7 @@ export default function Footer() {
       </div>
       <hr className="bg-primary" />
       <p className="flex justify-center p-5">
-        &copy; {new Date().getFullYear()} GreenField Dev
+        Designed & Developed by GreenField Dev &copy; {new Date().getFullYear()}
       </p>
     </footer>
   );
