d81caafa408ed890cf0994d7509e428e1c3a53ab
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-03-04 21:48:10 -0500

    updating sticky-scroll

--- src/app/components/sticky-scroll/sticky-scroll.tsx
+++ src/app/components/sticky-scroll/sticky-scroll.tsx
@@ -43,7 +43,7 @@ export const StickyScroll = ({
       animate={{
         backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
-      className="h-[30rem] lg:w-[1200px] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
+      className="h-[30rem] lg:w-[1200px] overflow-y-auto flex justify-center relative space-x-10 rounded-md my-10 mx-auto"
       ref={ref}
     >
       <div className="div relative flex items-start px-4">
