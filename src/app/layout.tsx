a07e264c11bf10387520b4b1caae3d318a6ce2e0
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-03-12 23:42:42 -0400

    removing the chatbot from the site for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Nav from "./components/navbar/navbar";
 import Script from "next/script";
 import { Metadata } from "next";
 import Head from "next/head";
+import { Roboto_Condensed } from "next/font/google";
 
 export const metadata: Metadata = {
   title: {
@@ -15,27 +16,28 @@ export const metadata: Metadata = {
     "C&M Mechanical HVAC provides services for all types of heating systems and cooling systems.",
   keywords:
     "HVAC, installation, air conditioning, heating, ductless, mitsubishi, NYSERDA, american standard dealer",
-    openGraph: {
-      title: 'C&M Mechanical Heating and Cooling',
-      description: 'C&M Mechanical HVAC provides services for all types of heating systems and cooling systems.',
-      url: 'https://www.cmmechanicalhvac.com',
-      siteName: 'C&M Mechanical Heating and Cooling',
-      images: [
-        {
-          url: 'https://www.cmmechanicalhvac.com/CMMechanical_logo.png', // Must be an absolute URL
-          width: 800,
-          height: 600,
-        },
-        {
-          url: 'https://www.cmmechanicalhvac.com/og-alt.png', // Must be an absolute URL
-          width: 1800,
-          height: 1600,
-          alt: 'My custom alt',
-        },
-      ],
-      locale: 'en_US',
-      type: 'website',
-    },
+  openGraph: {
+    title: "C&M Mechanical Heating and Cooling",
+    description:
+      "C&M Mechanical HVAC provides services for all types of heating systems and cooling systems.",
+    url: "https://www.cmmechanicalhvac.com",
+    siteName: "C&M Mechanical Heating and Cooling",
+    images: [
+      {
+        url: "https://www.cmmechanicalhvac.com/CMMechanical_logo.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://www.cmmechanicalhvac.com/og-alt.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -44,7 +46,7 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {  
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -53,8 +55,8 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <Script id="script">window.danteEmbed = &quot;https://chat.dante-ai.com/embed?kb_id=5821f9fb-5692-4892-918f-16e7828e21e6&token=d375d38d-98ac-4f86-82bc-8dc70c0c30cf&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=null&bubbleopen=false&quot;</Script> 
-      <Script src="https://chat.dante-ai.com/bubble-embed.js" />
+        {/*<Script id="script">window.danteEmbed = &quot;https://chat.dante-ai.com/embed?kb_id=5821f9fb-5692-4892-918f-16e7828e21e6&token=d375d38d-98ac-4f86-82bc-8dc70c0c30cf&modeltype=gpt-3.5-turbo&mode=false&bubble=true&image=null&bubbleopen=false&quot;</Script> 
+      <Script src="https://chat.dante-ai.com/bubble-embed.js" />*/}
       </body>
     </html>
   );
