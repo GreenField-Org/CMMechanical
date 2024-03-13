a4d154f3088ee70d4772d492f06136827cc844a9
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-03-12 23:40:08 -0400

    updating the financing page

--- src/app/financing/page.tsx
+++ src/app/financing/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@nextui-org/react";
 import Container from "../components/container/container";
+import Image from "next/image";
 
 export default function FinancingPage() {
   return (
     <div className="rounded-lg shadow-2xl bg-slate-100 p-3">
-      <div className="flex w-[375px] flex-col items-start gap-12 px-5 py-16 lg:w-full lg:gap-20 lg:px-16 lg:py-28">
+      <div className="flex w-[375px] flex-col items-start gap-12 px-5 py-16 lg:w-full lg:gap-20 lg:px-16 lg:pt-28">
         <div className="flex flex-col items-start gap-5 lg:gap-20 self-stretch">
           <div className="flex flex-col items-start gap-6 self-stretch lg:flex-[1_0_0]">
             <h1 className="self-stretch text-[40px] lg:text-[56px] not-italic font-bold leading-[120%]">
@@ -20,11 +21,10 @@ export default function FinancingPage() {
           </div>
         </div>
       </div>
-      <div className="flex w-[374px] flex-col items-start gap-12 px-5 py-16 lg:w-full lg:gap-20 lg:px-16 lg:py-28">
+      <div className="flex w-[374px] flex-col items-start gap-12 px-5 py-16 lg:w-full lg:gap-20 lg:px-16">
         <Container>
           <div className="flex flex-col items-start gap-2 lg:gap-6 self-stretch lg:flex-[1_0_0]">
             <div className="flex flex-col items-start gap-5 lg:gap-6 self-stretch">
-              <div className="w-20 h-20" />
               <div className="flex flex-col items-start gap-5 lg:gap-6 self-stretch">
                 <h2 className="self-stretch text-4xl lg:text-5xl not-italic font-bold leading-[120%]">
                   {
@@ -36,15 +36,28 @@ export default function FinancingPage() {
                     "Synchrony is one of the nation's premier consumer financial services companies. Our roots in consumer finance trace back to 1932, and today we are the largest provider of private label credit cards in the United States based on purchase volume and receivables."
                   }
                 </p>
-                <ul className="list-disc mx-5">
-                  <li>
-                    {"A simple application process, and fast credit decisions"}
-                  </li>
-                  <li>{"Unsecured, revolving credit lines"}</li>
-                  <li>{"Promotional financing options"}</li>
-                  <li>{"Convenient monthly payment options"}</li>
-                  <li>{"Online account management"}</li>
-                </ul>
+                <div className="flex flex-col lg:flex-row items-center gap-6 self-stretch lg:gap-8">
+                  <ul className="list-disc mx-5">
+                    <li>
+                      {
+                        "A simple application process, and fast credit decisions"
+                      }
+                    </li>
+                    <li>{"Unsecured, revolving credit lines"}</li>
+                    <li>{"Promotional financing options"}</li>
+                    <li>{"Convenient monthly payment options"}</li>
+                    <li>{"Online account management"}</li>
+                  </ul>
+                  <div className="my-0 mx-auto">
+                    <Image
+                      src="/synchrony.jpg"
+                      width={300}
+                      height={300}
+                      alt="Synchrony"
+                      className="shadow-2xl"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
             <div className="flex items-center gap-6 pt-4">
