62fe33b048ccca49fefe5c19a57196e7df8bf18f
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-04-24 10:13:54 -0400

    adding keywords for improved SEO

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports.SITE = {
   origin: 'https://cmmechanicalhvac.com',
   basePathname: '/',
   trailingSlash: false,
-
+  keywords: "quality mechanical systems heating contact services installation ductless hvac selkirk NY",
   title: 'C&M Mechanical Heating and Cooling Services',
   description: 'C&M Mechanical HVAC provides services for all types of heating systems and cooling systems.',
 };
