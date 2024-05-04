56b5e49ad3f7a24a3edb5d204c4906dc9972ab44
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-05-04 00:03:11 -0400

    adding metadata to ruud page

--- app/(pages)/products/ruud/page.tsx
+++ app/(pages)/products/ruud/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: `Ruud Products`,
+    alternates: {
+        canonical: '/products/ruud',
+    },
+  };
+  
 const Page = () => {
   return (
     <>
